76b50108310482f4bae77b9da600684640a60aa5
Matthew Pryor <matthewapryor@gmail.com>
2022-03-20 12:20:50 -0400

    really lost

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,19 @@
-var startButton = $( "a #start-button" );
+var startButton = $( "a #start" );
 var startContainer = $('div #start-quiz-wrapper')
-
+var question = $('div #hidden-q')
 
 // 1. Display starting screen
-// 2. User presses start quiz
+$(document).ready(function(){
+    // 2. User presses start quiz
+    startContainer.on("click", "#start", function() {
+        startContainer.hide()
+        setInterval(function(){
+            // 3. Timer starts counting down from 60 every second
+            time.textContent -= 1
+        }, 1000)
+        // 3. a) if user answers incorrectly, subract 5000 milliseconds from current time
+        // 4. a) When the timer reaches zero, remove any question displayed on the screen.
+        // 4. b) Display score and give user option to enter their initials for a high-score.
+    });
+})
 
-// Start quiz and delete the start quiz container
-startContainer.on("click", startButton, function() {
-    startContainer.remove()
-    setInterval(function(){
-        time.textContent -= 1
-    }, 1000)
-});
-
-
-// 3. Timer starts counting down from 60
-// 3. a) if user answers incorrectly, subract 5000 milliseconds from current time
-// 3. b) if user answers correctly, display the next question
-// 4. When the timer reaches zero, remove any question displayed on the screen.
-// 5. Display score and give user option to enter their initials for a high-score.
