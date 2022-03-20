9df993c74994d6c401f6bb3c55b7d3ec1713e65c
Matthew Pryor <matthewapryor@gmail.com>
2022-03-20 14:30:30 -0400

    getting back on track. timer working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,36 @@
 var startButton = $( "a #start" );
 var startContainer = $('div #start-quiz-wrapper')
-var question = $('div #question')
+var question = $('#question')
+// var time = $('#time')
 
 
 // 1. Display starting screen
 $(document).ready(function(){
-    question.hide()
     // 2. User presses start quiz
     startContainer.on("click", "#start", function() {
         startContainer.addClass('invisible')
         question.removeClass('invisible')
-        setInterval(function(){
-            // 3. Timer starts counting down from 60 every second
-            time.textContent -= 1
-
-            if (content.textContent === 0) {
-                clearInterval();
-            }
-
-
-        }, 10)
+        handleTimeUp();
+        
         // 3. a) if user answers incorrectly, subract 5000 milliseconds from current time
         // 4. a) When the timer reaches zero, remove any question displayed on the screen.
         // 4. b) Display score and give user option to enter their initials for a high-score.
     });
 })
 
+// create a function handle time up and when all questions answered
+
+var handleTimeUp = function() {
+    var timeInterval = setInterval(function(){
+        // 3. Timer starts counting down from 60 every second
+        time.textContent -= 1
+        
+        if (time.textContent === "0") {
+            console.log('time is up!')
+            
+
+            clearInterval(timeInterval);
+            question.removeClass('invisible')
+        }
+    }, 10)
+}
