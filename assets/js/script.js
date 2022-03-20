09872b65c102be590b1ce3984e6228dd343b8400
Matthew Pryor <matthewapryor@gmail.com>
2022-03-20 13:19:49 -0400

    stuck on making the timer stop at zero

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,25 @@
 var startButton = $( "a #start" );
 var startContainer = $('div #start-quiz-wrapper')
-var question = $('div #hidden-q')
+var question = $('div #question')
+
 
 // 1. Display starting screen
 $(document).ready(function(){
+    question.hide()
     // 2. User presses start quiz
     startContainer.on("click", "#start", function() {
-        startContainer.hide()
+        startContainer.addClass('invisible')
+        question.removeClass('invisible')
         setInterval(function(){
             // 3. Timer starts counting down from 60 every second
             time.textContent -= 1
-        }, 1000)
+
+            if (content.textContent === 0) {
+                clearInterval();
+            }
+
+
+        }, 10)
         // 3. a) if user answers incorrectly, subract 5000 milliseconds from current time
         // 4. a) When the timer reaches zero, remove any question displayed on the screen.
         // 4. b) Display score and give user option to enter their initials for a high-score.
