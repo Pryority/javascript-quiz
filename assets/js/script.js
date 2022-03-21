df6d4c67dc8622fd4944191c09a1e2eafd127537
Matthew Pryor <matthewapryor@gmail.com>
2022-03-20 22:04:07 -0400

    add 5 questions and structured them so the answer takes you to the next question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 var startButton = $( "a #start" );
 var startContainer = $('div #start-quiz-wrapper')
 var question1 = $('#question1')
+var question2 = $('#question2')
+var question3 = $('#question3')
+var question4 = $('#question4')
+var question5 = $('#question5')
 var score = $('div #score')
 var correct = $('#correct-prompt')
 var incorrect = $('#incorrect-prompt')
@@ -16,7 +20,7 @@ var handleTimeUp = function() {
             
 
             clearInterval(timeInterval);
-            question1.show()
+            startContainer.show()
         }
     }, 1000)
 }
@@ -26,6 +30,10 @@ $(function(){
     $('#correct-prompt').hide()
     $('#incorrect-prompt').hide()
     question1.hide()
+    question2.hide()
+    question3.hide()
+    question4.hide()
+    question5.hide()
     score.hide()
 
     // 2. User presses start quiz
@@ -36,7 +44,27 @@ $(function(){
         question1.show()
         question1.on('click', '#answer', function(){
             question1.hide()
+            question2.show()
+            question2.on('click', '#answer', function(){
+                question2.hide()
+                question3.show()
+                question3.on('click', '#answer', function(){
+                    question3.hide()
+                    question4.show()
+                    question4.on('click', '#answer', function(){
+                        question4.hide()
+                        question5.show()
+                        question5.on('click', '#answer', function(){
+                            question5.hide()
+                        })
+                    })
+                    
+                })
+                
+            })
+            
         })
+        
         // Function to handle end of game from time out
         handleTimeUp();
         
